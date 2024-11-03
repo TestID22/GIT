37578dfcbceef37bc2e8f59d52d9467c67ff1d52
VSabadash <vitaliiseptember@gmail.com>
2024-11-03 10:31:51 +0200

    new UI: VSC + commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var registrationButton = document.getElementById('registrationButton');
-    var registrationForm = document.getElementById('registrationForm');
-  
-    registrationButton.addEventListener('click', function() {
-      registrationForm.classList.toggle('hidden');
-    });
-  });
-  
-  function fallLetters() {
-    var letters = document.querySelectorAll("#letterContainer span");
+document.addEventListener('DOMContentLoaded', function () {
+  var registrationButton = document.getElementById('registrationButton');
+  var registrationForm = document.getElementById('registrationForm');
 
-    letters.forEach(function(letter, index) {
-        
-        setTimeout(function() {
-            letter.style.transform = "translateY(1400px)";
-            letter.style.color = "orange";
-        }, index * 100); 
-    });
+  registrationButton.addEventListener('click', function () {
+    registrationForm.classList.toggle('hidden');
+  });
+});
+
+function fallLetters() {
+  var letters = document.querySelectorAll("#letterContainer span");
+
+  letters.forEach(function (letter, index) {
+
+    setTimeout(function () {
+      letter.style.transform = "translateY(1400px)";
+      letter.style.color = "orange";
+    }, index * 100);
+  });
+}
+
+function toggleText(id) {
+  const textElement = document.getElementById(id);
+  textElement.classList.toggle('hidden');
 }
